1f07bf540a3b2d2dfb2857a47d6fe644bc682799
sankaranarayanan <esankaranarayanan@gmail.com>
2012-12-19 12:06:53 +0530

    fixed signin redirect path

--- app/controllers/forem/application_controller.rb
+++ app/controllers/forem/application_controller.rb
@@ -14,14 +14,13 @@ class Forem::ApplicationController < ApplicationController
 
   def authenticate_forem_user
     if !forem_user
-      session["user_return_to"] = request.fullpath
       flash.alert = t("forem.errors.not_signed_in")
       devise_route = "new_#{Forem.user_class.to_s.underscore}_session_path"
       sign_in_path = Forem.sign_in_path ||
         (main_app.respond_to?(devise_route) && main_app.send(devise_route)) ||
         (main_app.respond_to?(:sign_in_path) && main_app.send(:sign_in_path))
       if sign_in_path
-        redirect_to sign_in_path
+        redirect_to "#{sign_in_path}?origin=#{request.fullpath}"
       else
         raise "Forem could not determine the sign in path for your application. Please do one of these things:
 
